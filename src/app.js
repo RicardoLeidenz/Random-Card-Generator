786537692f986c9ff7b7312a82179a6d812750fa
Ricardo Leidenz <139507934+RicardoLeidenz@users.noreply.github.com>
2025-07-28 23:41:25 +0000

    Added card generator and random button

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+ newCard();
 };
+
+function newCard(){
+  const card = document.getElementById("newCard");
+  let suit = getSuit();
+  
+  card.innerHTML = `<h1 class="row float-start mb-5">${suit}</h1>
+                    <h1 class="row justify-content-center mt-5">${getFace()}</h1>
+                    <h1 class="row mt-4 float-end">${suit}</h1>`;
+}
+
+function getSuit(){
+  const suits = ["♦","♥","♠","♣"]
+  let index = Math.floor(Math.random() * 4);
+  return suits[index];
+}
+
+function getFace(){
+  const cards = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+  let index = Math.floor(Math.random() * 13);
+  return cards[index];
+}
