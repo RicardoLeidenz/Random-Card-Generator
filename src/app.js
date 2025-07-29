81dc84da353cee4199148106a09a75e4e7847d00
Ricardo Leidenz <139507934+RicardoLeidenz@users.noreply.github.com>
2025-07-29 00:20:54 +0000

    cleaned up code and linked

--- src/app.js
+++ src/app.js
@@ -1,27 +1,19 @@
 import "bootstrap";
 import "./style.css";
 
-window.onload = function() {
- newCard();
-};
+const cards = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+const suits = ["diamonds","hearts","spades","clubs"];
+const symbols = {diamonds:"♦",hearts:"♥",spades:"♠",clubs:"♣"};
 
-function newCard(){
-  const card = document.getElementById("newCard");
-  let suit = getSuit();
-  
-  card.innerHTML = `<h1 class="row float-start mb-5">${suit}</h1>
-                    <h1 class="row justify-content-center mt-5">${getFace()}</h1>
-                    <h1 class="row mt-4 float-end">${suit}</h1>`;
+window.newCard = function newCard(){
+  const card = document.getElementById("card");
+  let face = cards[Math.floor(Math.random() * cards.length)];
+  let suit = suits[Math.floor(Math.random() * suits.length)];
+  card.innerHTML = `<div class="col-1 bg-white mx-2 rounded ${suit}">
+                      <h1 class="row float-start mb-5">${symbols[suit]}</h1>
+                      <h1 class="row justify-content-center mt-5">${face}</h1>
+                      <h1 class="row mt-4 float-end">${symbols[suit]}</h1>
+                    </div>`;
 }
 
-function getSuit(){
-  const suits = ["♦","♥","♠","♣"]
-  let index = Math.floor(Math.random() * 4);
-  return suits[index];
-}
-
-function getFace(){
-  const cards = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
-  let index = Math.floor(Math.random() * 13);
-  return cards[index];
-}
+newCard();
